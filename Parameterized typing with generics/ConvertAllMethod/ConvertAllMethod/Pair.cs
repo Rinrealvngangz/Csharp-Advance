624dcc9b718fe0e7b30d7eafaa84578341df43d8
rinrealvngangz <tuannmctk42@gmail.com>
2021-12-29 23:58:19 +0700

    add generic class represen a pair of value

---
+++ Parameterized typing with generics/ConvertAllMethod/ConvertAllMethod/Pair.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Collections.Generic;
+
+
+namespace ConvertAllMethod
+{
+    public sealed class Pair<T1,T2> : IEquatable<Pair<T1,T2>>
+    {
+        private  static  readonly IEqualityComparer<T1> FirstComparer =
+            EqualityComparer<T1>.Default;
+        
+        private  static  readonly IEqualityComparer<T2> SecondComparer =
+            EqualityComparer<T2>.Default;
+
+        private readonly T1 first;
+        private readonly T2 second;
+
+        public Pair(T1 first,T2 second)
+        {
+            this.first = first;
+            this.second = second;
+        }
+        
+        public T1 Frist
+        {
+            get { return first; }
+        }
+
+        public T2 Second
+        {
+            get { return second; }
+        }
+        
+        public bool Equals(Pair<T1, T2>? other)
+        {
+            return other != null &&
+                 //  this.first == other.Frist : error
+                  FirstComparer.Equals(this.first, other.Frist) &&
+                   SecondComparer.Equals(this.second, other.Second);
+        }
+
+        public override bool Equals(object? obj)
+        {
+            return Equals(obj as Pair<T1, T2>);
+        }
+
+        public override int GetHashCode()
+        {
+           return FirstComparer.GetHashCode(first) * 37 + 
+                  SecondComparer.GetHashCode(second);
+
+        }
+    }
+}
--- Parameterized typing with generics/ConvertAllMethod/ConvertAllMethod/Program.cs
+++ Parameterized typing with generics/ConvertAllMethod/ConvertAllMethod/Program.cs
@@ -9,10 +9,26 @@ namespace ConvertAllMethod
         {
             return Math.Sqrt(x);
         }
+
+        static int CompareToDefault<T>(T value) where T : IComparable<T>
+        {
+            
+            return value.CompareTo(default(T));
+        }
+
+        static bool AreReferencesEqual<T>(T first, T second)
+            where T : class
+        {
+            return first == second;
+        }
+        
+        
         
         static void Main(string[] args)
         {
-            List<int> intergers = new List<int>();
+            // ConverAll method
+            
+           /* List<int> intergers = new List<int>();
             intergers.Add(1);
             intergers.Add((2));
             intergers.Add(3);
@@ -23,7 +39,36 @@ namespace ConvertAllMethod
            foreach (var d in doubles)
            {
                Console.WriteLine(d);
-           }
+           }*/
+           
+           
+       
+        // Comparing a given value to the default in a generic way
+        
+        /*   Console.WriteLine(CompareToDefault("x"));
+           
+           Console.WriteLine(CompareToDefault(10));
+           
+           Console.WriteLine(CompareToDefault(0));
+           
+           Console.WriteLine(CompareToDefault(-10));
+           
+           Console.WriteLine(CompareToDefault(DateTime.MinValue));*/
+           
+      
+       /* string name = "jon";
+        string intro1 = "My name is " + name;
+        string intro2 = "My name is " + name;
+       Console.WriteLine(intro1 == intro2);
+       Console.WriteLine(AreReferencesEqual(intro1,intro2));*/
+
+       // Generic class representing a pair of values
+       Pair<int, string> pair = new Pair<int, string>(10, "value");
+
+       pair.Equals(new Pair<int, string>(10, "value"));
+       
+       pair.GetHashCode();
+
         }
     }
 }
