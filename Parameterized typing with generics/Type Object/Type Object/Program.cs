293aaf5153730ca3908ed459bc6907d21287fd6c
rinrealvngangz <tuannmctk42@gmail.com>
2021-12-31 11:50:09 +0700

    add type object in System.Type

---
+++ Parameterized typing with generics/Type Object/Type Object/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+
+namespace TypeObject
+{
+    class Program
+    {
+        static void PrintTypeParameter<T>()
+        {
+            Console.WriteLine(typeof(T));
+        }
+        static void Main(string[] args)
+        {
+            string listTypeName = "System.Collections.Generic.List`1";
+            Type defByName = Type.GetType(listTypeName);
+            Type closedByName = Type.GetType(listTypeName + "[System.String]");
+            Type closedByMethod = defByName.MakeGenericType(typeof(string));
+            Type closedByTypeof = typeof(List<string>);
+            
+            Console.WriteLine(closedByMethod == closedByName);
+            Console.WriteLine(closedByName == closedByTypeof);
+   
+            Type defByTypeof = typeof(List<>);
+            Type defByMethod = closedByName.GetGenericTypeDefinition();
+            
+            Console.WriteLine(defByMethod == defByName);
+            Console.WriteLine(defByName == defByMethod);
+           object dynamic =   Activator.CreateInstance(closedByMethod);
+        
+           Console.WriteLine(closedByMethod.FullName);
+           var objDynamic = dynamic as List<string>;
+           objDynamic.Add("a");
+           objDynamic.Add("b");
+           Console.WriteLine(objDynamic[0]);
+
+        }
+    }
+}
