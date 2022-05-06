c2f9b4d2ffdce0ebce62641a05bfcaa955efb6ff
rinrealvngangz <tuannmctk42@gmail.com>
2022-05-06 12:15:11 +0700

    nghien cuu ve yield break vs try/finally

--- Iterator/Iterator/Program.cs
+++ Iterator/Iterator/Program.cs
@@ -34,3 +34,78 @@ while (true)
     Console.WriteLine("Fetching Current...");
     Console.WriteLine("... Current result={0}",iterator.Current);
 }
+
+// yield break;
+static IEnumerable<int> CountWithTimeLimit(DateTime limit)
+{
+    for (int i = 0; i < 10; i++)
+    {
+       if (DateTime.Now >= limit)
+        {
+            yield break;
+        }
+       
+         yield return i;
+    }
+}
+
+// Yield break try/Finally
+static IEnumerable<int> CountWithTimeLimitWithTryFinal(DateTime limit)
+{
+    try
+    {
+        for (int i = 0; i <= 10; i++)
+        {
+          
+            if (DateTime.Now >= limit)
+            {
+                yield break;
+            }
+
+            yield return i;
+        }
+    }
+    finally
+    {
+        Console.WriteLine("Stopping!");
+    }
+    
+}
+
+
+DateTime stop = DateTime.Now.AddSeconds(2);
+
+foreach (int i in CountWithTimeLimitWithTryFinal(stop))
+{
+    Console.WriteLine("Received {0}",i);
+    if (i > 3)
+    {
+        Console.WriteLine("Retunring");
+        return;
+    }
+    await Task.Delay(300);//300 milisecond tương đương 6 lần lặp - thời gian càng ít số lần lặp càng 
+}
+
+IEnumerable<int> iterable1 = CountWithTimeLimitWithTryFinal(stop);
+IEnumerator<int> iterator1 = iterable1.GetEnumerator();
+
+/*while (true)
+{
+    bool result = iterator1.MoveNext();
+ 
+    Console.WriteLine(result);
+    Console.WriteLine("Received {0}",iterator1.Current);
+    if (iterator.Current > 3)
+    {
+        Console.WriteLine("Retunring");
+        return;
+    } 
+    await Task.Delay(300);
+}*/
+Console.WriteLine("Received {0}",iterator1.Current);
+bool result1 = iterator1.MoveNext();
+Console.WriteLine(result1);
+Console.WriteLine("Received {0}",iterator1.Current);
+
+iterator.MoveNext();
+Console.WriteLine("Received {0}",iterator.Current);
